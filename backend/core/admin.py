d2b8e1436be5f67211f43b94c0d3c9dfe6d89b77
revocajana <revocajana@gmail.com>
2026-05-31 01:41:20 +0300

    what is to be seen in the admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from .models import (
+    Country,
+    Zone,
+    Region,
+    District,
+    Ward,
+    School,
+    User,
+    Parent,
+)
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+    search_fields = ("name", "code")
+
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    list_filter = ("country",)
+    search_fields = ("name",)
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name", "zone")
+    list_filter = ("zone",)
+    search_fields = ("name",)
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ("name", "region")
+    list_filter = ("region",)
+    search_fields = ("name",)
+
+
+@admin.register(Ward)
+class WardAdmin(admin.ModelAdmin):
+    list_display = ("name", "district")
+    list_filter = ("district",)
+    search_fields = ("name",)
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "registry_number",
+        "ownership_type",
+        "country",
+        "zone",
+        "region",
+        "district",
+        "ward",
+    )
+    list_filter = (
+        "ownership_type",
+        "country",
+        "zone",
+        "region",
+        "district",
+        "ward",
+    )
+    search_fields = ("name", "registry_number")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "role", "school")
+    list_filter = ("role", "school")
+    search_fields = ("username", "email")
+
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "username", "phone", "email")
+    search_fields = ("full_name", "username")
