9e8bb82a258b3847f12069f819103ed49e45f156
revocajana <revocajana@gmail.com>
2026-05-31 15:06:23 +0300

    students in admin dashboard

--- backend/students/admin.py
+++ backend/students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "gender", "school", "parent", "student_id")
+    list_filter = ("gender", "school")
+    search_fields = ("first_name", "last_name", "student_id")
+
