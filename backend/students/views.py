02492f7a00b941858b4ba7122d6a3c06658c0c4d
revocajana <revocajana@gmail.com>
2026-06-04 15:36:12 +0300

    students-parents views

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Student, Parent
+from .serializers import StudentSerializer, ParentSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.select_related('school', 'parent').all()
+    serializer_class = StudentSerializer
+
+
+class ParentViewSet(viewsets.ModelViewSet):
+    queryset = Parent.objects.all()
+    serializer_class = ParentSerializer
