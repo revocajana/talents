7bb94d35c6d74f3935583411238332251c6b47d7
revocajana <revocajana@gmail.com>
2026-06-04 15:27:19 +0300

    adding the api urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from core.views import (
+    CountryViewSet,
+    ZoneViewSet,
+    RegionViewSet,
+    DistrictViewSet,
+    WardViewSet,
+    SchoolViewSet,
+    UserViewSet,
+)
+from students.views import StudentViewSet, ParentViewSet
+from competitions.views import CompetitionViewSet, CompetitionParticipationViewSet
+
+router = DefaultRouter()
+router.register('countries', CountryViewSet)
+router.register('zones', ZoneViewSet)
+router.register('regions', RegionViewSet)
+router.register('districts', DistrictViewSet)
+router.register('wards', WardViewSet)
+router.register('schools', SchoolViewSet)
+router.register('users', UserViewSet)
+router.register('students', StudentViewSet)
+router.register('parents', ParentViewSet)
+router.register('competitions', CompetitionViewSet)
+router.register('participations', CompetitionParticipationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
