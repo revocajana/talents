b6e7e887147f8405d85aefb558394a4c5268c046
revocajana <revocajana@gmail.com>
2026-06-04 15:35:07 +0300

    core views

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Country, Zone, Region, District, Ward, School, User
+from .serializers import (
+    CountrySerializer,
+    ZoneSerializer,
+    RegionSerializer,
+    DistrictSerializer,
+    WardSerializer,
+    SchoolSerializer,
+    UserSerializer,
+)
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class ZoneViewSet(viewsets.ModelViewSet):
+    queryset = Zone.objects.select_related('country').all()
+    serializer_class = ZoneSerializer
+
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = Region.objects.select_related('zone').all()
+    serializer_class = RegionSerializer
+
+
+class DistrictViewSet(viewsets.ModelViewSet):
+    queryset = District.objects.select_related('region').all()
+    serializer_class = DistrictSerializer
+
+
+class WardViewSet(viewsets.ModelViewSet):
+    queryset = Ward.objects.select_related('district').all()
+    serializer_class = WardSerializer
+
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.select_related('country', 'zone', 'region', 'district', 'ward').all()
+    serializer_class = SchoolSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.select_related('school').all()
+    serializer_class = UserSerializer
