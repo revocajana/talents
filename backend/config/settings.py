8ab080065eb7fa0ed4901ad1a0a7fe275ad44ece
revocajana <revocajana@gmail.com>
2026-08-18 22:22:30 +0300

    default pagenation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,6 +67,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 MIDDLEWARE = [
