1c13908318c35dab9e2473df095781d91ace96ce
revocajana <revocajana@gmail.com>
2026-08-30 21:59:31 +0300

    school ownership

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -111,6 +111,7 @@ export const createWard = (data) => api.post('/wards/', data);
 export const updateWard = (id, data) => api.put(`/wards/${id}/`, data);
 export const deleteWard = (id) => api.delete(`/wards/${id}/`);
 export const getSchools = (params = {}) => api.get('/schools/', { params });
+export const getSchoolOwnershipTypes = (params = {}) => api.get('/school-ownership-types/', { params }); // New endpoint for school ownership types
 export const getAllCountries = (params = {}) => getAllRecords('/countries/', params);
 export const getAllZones = (params = {}) => getAllRecords('/zones/', params);
 export const getAllRegions = (params = {}) => getAllRecords('/regions/', params);
