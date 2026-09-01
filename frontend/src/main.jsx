dad729a3f8f25fd97f537d0c3823da31fa555ded
revocajana <revocajana@gmail.com>
2026-09-02 01:42:12 +0300

    talanta in the browser tab

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logo from './assets/Logo1.png'
+
+document.title = 'Talanta'
+const favicon = document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.href = logo
+document.head.appendChild(favicon)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
